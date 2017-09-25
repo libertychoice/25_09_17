7e3497871cbb19ae51437bb012dedcd9ef0d39c4
Student <test@test.com>
2017-09-25 21:34:30 +0300

    start js

---
+++ test_js/25_09_17/js/my_js.js
@@ -0,0 +1,20 @@
+'use strict';
+//line comment
+
+/*
+block comment
+
+*/
+
+console.log("test");
+let d = 5;
+
+{
+    let d = 7;
+    console.log(d);
+}
+console.log(d);
+
+var x; //function
+let y; //2015  block
+const z=3; //2015 if simple  const else variable block
