8211b1f198d917e8755bc347be713bf3d5ec2415
Student <test@test.com>
2017-09-28 22:00:43 +0300

    add functions

--- test_js/25_09_17/js/my_js.js
+++ test_js/25_09_17/js/my_js.js
@@ -6,6 +6,112 @@ block comment
 
 */
 
+//call
+//apply
+
+let obj2 = {
+    red:"green",
+}
+
+
+let obj = {
+    red:"color",
+    getRed: function(a,s){
+        return this.red;
+    },
+    getRed2:()=>{
+        return this.red;
+    }
+    
+    
+}
+
+function test(){
+    for(let cnt=0;cnt<=1000;cnt++){
+        cnt%21===0 ? console.log(cnt) : undefined;
+    }
+
+}
+
+function test1(){
+    let s = "fafaoofa"
+    let i=0;
+    let y=0;
+    for (let k=0;k<=s.length;k++){
+        if(s[k]==="a")
+        {
+            i++
+        }
+        else if(s[k]==="o"){
+            y++
+        }
+    }
+    console.log(i)
+    console.log(y)
+}
+
+function testIF(a){
+    if ((a>5)&&(a<9)){
+        console.log(">5");
+    }else if(true){
+        console.log("other");
+    }
+}
+
+
+function testSW(a){
+    let res;
+    switch(a){
+        case 3:
+        case 4:
+            res=true
+            break;
+        case 5:
+            res=true
+            break;
+        default:
+            res=false
+    }
+    return res
+}
+
+
+function loops(){
+  /*  let x=0
+    while(x<9){
+        console.log(x)
+        x++;
+    }
+
+
+    do{
+
+    }while(cond)
+*/
+    let obj={
+        test:"we",
+        red:23
+    }
+    let arr = [1,2,3,,,5,43,];
+    arr.length=20;
+
+    for(let cnt=0;cnt<=arr.length;cnt+=2){
+        console.log(arr[cnt])
+    }
+    console.log("testtest")
+
+    for(let k in arr){
+        console.log(arr[k]);
+    }
+
+    for (let k of obj){
+        console.log(k)
+    }
+}
+
+
+
+
 console.log("test");
 let d = 5;
 
